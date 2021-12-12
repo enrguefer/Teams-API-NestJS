4a91401b8d13b9d32e529cf79ae997d8c7e04495
Enrique Guerrero Fernández <enriqueguefer@gmail.com>
2021-12-12 19:40:40 +0100

    Se elimina el controlador y servicio app creado por defecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from 'src/controlers/app.controller';
-import { AppService } from 'src/services/app.service';
 import { TeamsModule } from './modules/teams/teams.module';
 
 @Module({
   imports: [TeamsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/services/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return '¡Bienvenido a la Api de Equipos! Autor: Enrique Guerrero';
-  }
-}
