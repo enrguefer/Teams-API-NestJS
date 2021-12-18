2bf6321f6df7ef9585700aafd49fefe62199211c
Enrique Guerrero Fernández <enriqueguefer@gmail.com>
2021-12-18 14:55:49 +0100

    Conexión con la Mongo remota

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TeamsModule } from './modules/teams/teams.module';
 
 @Module({
-  imports: [TeamsModule],
+  imports: [
+    TeamsModule,
+    MongooseModule.forRoot('mongodb+srv://<USER>:<PASSWORD>@cluster0.97vvi.mongodb.net/teams-api-nestjs?retryWrites=true&w=majority')
+  ],
   controllers: [],
   providers: [],
 })
