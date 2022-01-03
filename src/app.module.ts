b2bcc4c4169cf9480e2a08db963ecce07db967f9
Enrique Guerrero Fernández <enriqueguefer@gmail.com>
2022-01-03 20:24:06 +0100

    Instalación de la librería automapper

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { APP_FILTER } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ApiExceptionHandler } from './exceptions/api.exception.handler';
 import { TeamsModule } from './modules/teams/teams.module';
+import { AutomapperModule } from '@automapper/nestjs';
+import { classes } from '@automapper/classes';
 
 @Module({
   imports: [
     TeamsModule,
-    MongooseModule.forRoot('mongodb+srv://<USER>:<PASSWORD>@cluster0.97vvi.mongodb.net/teams-api-nestjs?retryWrites=true&w=majority')
+    MongooseModule.forRoot('mongodb+srv://<USER>:<PASSWORD>@cluster0.97vvi.mongodb.net/teams-api-nestjs?retryWrites=true&w=majority'),
+    AutomapperModule.forRoot( { options: [{ name: 'Mapper', pluginInitializer: classes }], singular: true }),
   ],
   controllers: [],
   providers: [
